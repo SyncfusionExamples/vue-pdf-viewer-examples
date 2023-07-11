484b9d68753a797144e04986f3f947d0629f4400
SaravanaPriya31 <Saravana31>
2023-07-11 23:38:37 +0530

    837867-azure

---
+++ Save and Load/Open-Save-PDF-document-Azure-Blob-Storage/Open-Save-PDF-document-Azure-Blob-Storage/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <ejs-pdfviewer
+      id="pdfViewer"
+      :serviceUrl="serviceUrl"
+      :documentPath="documentPath"
+    >
+    </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import { PdfViewerPlugin, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner } from '@syncfusion/ej2-vue-pdfviewer';
+  Vue.use(PdfViewerPlugin);
+  export default {
+    name: 'app',
+    data() {
+      return {
+        // Replace the "localhost:44396" with the actual URL of your server
+        serviceUrl: "https://localhost:44396/pdfviewer",
+        documentPath: "PDF_Succinctly.pdf"
+      };
+    },
+    provide: {
+      PdfViewer: [Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner]
+    }
+  }
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
