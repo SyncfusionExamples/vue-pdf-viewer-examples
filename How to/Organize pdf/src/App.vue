bdc2491750e38f6210bdd5c5629dc52b39393f3a
SaravanaPriya31 <Saravana31>
2024-03-04 11:17:20 +0530

    865587: Organize pdf sample commit

---
+++ How to/Organize pdf/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <ejs-pdfviewer
+      id="pdfViewer"
+      :documentPath="documentPath"
+      :resourceUrl="resourceUrl"
+      :documentLoad="documentLoad">
+    </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import { PdfViewerPlugin, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner, PageOrganizer } from '@syncfusion/ej2-vue-pdfviewer';
+  Vue.use(PdfViewerPlugin);
+  var viewer;
+  export default {
+    name: 'app',
+    data() {
+      return {
+        documentPath: "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
+        resourceUrl: 'https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib'
+      };
+    },
+    provide: {
+      PdfViewer: [Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields, FormDesigner, PageOrganizer]
+    },
+    methods: {
+      documentLoad: function (args) {
+        var isInitialLoading = true;
+        viewer = this.$refs.pdfviewer.ej2Instances;
+        if (isInitialLoading) {
+          viewer.isPageOrganizerOpen = true;
+          isInitialLoading = false;
+        } else {
+          viewer.isPageOrganizerOpen = false;
+        }
+      },
+    }    
+  }
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
