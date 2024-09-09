b6b645a42db57794557a3c0cae0e05a087d674f7
Vincentsf4657 <vincentkumar.l@syncfusion.com>
2024-09-09 20:05:58 +0530

    906190: UGDocumentation Samples

---
+++ How to/Customization of PrintQuality/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <ejs-pdfviewer
+      id="pdfViewer"
+      :documentPath="documentPath"
+      :serviceUrl="serviceUrl"
+      :printScaleFactor=0.5
+       >
+    </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { PdfViewerPlugin, Toolbar, Magnification, Navigation, 
+         Annotation, LinkAnnotation, BookmarkView, ThumbnailView, 
+         Print, TextSelection, TextSearch, FormFields, FormDesigner, PageOrganizer } from '@syncfusion/ej2-vue-pdfviewer';
+  Vue.use(PdfViewerPlugin);
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        serviceUrl: "https://services.syncfusion.com/vue/production/api/pdfviewer",
+        documentPath: "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
+      };
+    },
+
+    provide: {
+    PdfViewer: [ Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, 
+                 Print, TextSelection, TextSearch, Annotation, FormDesigner, FormFields, PageOrganizer ]},  
+   
+}
+
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+</style>
