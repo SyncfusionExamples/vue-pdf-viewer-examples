fc7357f4ead04be6d257a4c1d19444127b508b94
GiriSF4292 <giridharmalayakonda.m@syncfusion.com>
2024-09-20 13:10:38 +0530

    897679: committed the changes regarding text search sample

---
+++ How to/TextSearch/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <button v-on:click="findTextBounds">FindTextBounds</button>
+      <ejs-pdfviewer
+        id="pdfViewer"
+        ref="pdfviewer"
+        :documentPath="documentPath"
+        :resourceUrl="resourceUrl">
+      </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { PdfViewerPlugin, Toolbar, Magnification, Navigation, 
+         LinkAnnotation, BookmarkView, Annotation, ThumbnailView, 
+         Print, TextSelection, TextSearch, FormFields, FormDesigner,PageOrganizer  } from '@syncfusion/ej2-vue-pdfviewer';
+Vue.use(PdfViewerPlugin);
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      resourceUrl:"https://cdn.syncfusion.com/ej2/24.1.41/dist/ej2-pdfviewer-lib",
+      documentPath:"https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf"
+    };
+  },
+  provide: {
+    PdfViewer: [ Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, Annotation, 
+                 ThumbnailView, Print, TextSelection, TextSearch, FormFields, FormDesigner, PageOrganizer ]},
+
+  methods: {
+    findTextBounds: function () {
+      var viewer = this.$refs.pdfviewer.ej2Instances;
+      // Find and get the bounds of a text
+      console.log(viewer.textSearch.findText('adobe', false));
+      // Find and get the bounds of a text on the desired page
+      console.log(viewer.textSearch.findText('adobe', false, 7));
+      // Find and get the bounds of the list of text
+      console.log(viewer.textSearch.findText(['adobe', 'pdf'], false));
+      // Find and get the bounds of the list of text on desired page
+      console.log(viewer.textSearch.findText(['adobe', 'pdf'], false, 7));
+    }
+  }
+}
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+</style>
