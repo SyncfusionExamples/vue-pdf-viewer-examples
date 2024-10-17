eca4092937cc0bc7dc54c79c0bf51b074ecedc3e
Dharshana4609 <dharshana.rameshgupta@syncfusion.com>
2024-10-17 19:15:51 +0530

    915494: Resolved dependent bot issue for handwritten signature and print quality samples

--- How to/Customization of PrintQuality/src/App.vue
+++ How to/Customization of PrintQuality/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <ejs-pdfviewer
-      id="pdfViewer"
-      :documentPath="documentPath"
-      :serviceUrl="serviceUrl"
-      :printScaleFactor="printScaleFactor"
-       >
-    </ejs-pdfviewer>
-  </div>
+  <ejs-pdfviewer
+    id="pdfViewer"
+    :documentPath="documentPath"
+    :serviceUrl="serviceUrl"
+    :printScaleFactor="printScaleFactor"
+    >
+  </ejs-pdfviewer>
 </template>
 
 <script>
-import Vue from 'vue';
-import { PdfViewerPlugin, Toolbar, Magnification, Navigation, 
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, 
          Annotation, LinkAnnotation, BookmarkView, ThumbnailView, 
          Print, TextSelection, TextSearch, FormFields, FormDesigner, PageOrganizer } from '@syncfusion/ej2-vue-pdfviewer';
-  Vue.use(PdfViewerPlugin);
 
   export default {
-    name: 'app',
+    name: 'App',
+
+    components: {
+      "ejs-pdfviewer": PdfViewerComponent
+    },
     data() {
       return {
-        serviceUrl: "https://services.syncfusion.com/vue/production/api/pdfviewer",
+        serviceUrl: "https://localhost:44396/pdfviewer",
         documentPath: "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf",
         //By Changing the printScaleFactor value we can adjust the quality of the PDF file.
         printScaleFactor: 0.5,
