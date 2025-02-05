63f9c4ad22e87288e7454e3ecc610f0c6aca31bd
Uma-SF4267 <umamageshwari.shanmugam@syncfusion.com>
2025-02-05 14:09:24 +0530

    931453: How to reference resource URL locally in the PDF Viewer without using CDN links?

---
+++ How to/Refer resource url locally/quickstart/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <ejs-pdfviewer 
+    id="pdfViewer" 
+    :resourceUrl="resourceUrl" 
+    :documentPath="documentPath">
+  </ejs-pdfviewer>
+</template>
+
+<script>
+  import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, 
+           ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormDesigner, FormFields, PageOrganizer } from '@syncfusion/ej2-vue-pdfviewer';
+
+  export default {
+    name: 'App',
+
+    components: {
+      "ejs-pdfviewer": PdfViewerComponent
+    },
+
+    data() {
+      return {
+        resourceUrl: window.location.origin + "/asset/ej2-pdfviewer-lib",
+        documentPath: window.location.origin + "/asset/pdfsuccinctly.pdf"
+      };
+    },
+    provide: {
+      PdfViewer: [ Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView,
+                   Print, TextSelection, TextSearch, Annotation, FormDesigner, FormFields, PageOrganizer ]
+    }
+  }
+</script>
+
+<style>
+  @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';  
+  @import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';  
+  @import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-lists/styles/material.css';
+  @import '../node_modules/@syncfusion/ej2-pdfviewer/styles/material.css';
+</style>
