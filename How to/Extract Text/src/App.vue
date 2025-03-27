250518daae64e715b977fe6e3c5fc6325510754a
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-03-27 11:36:19 +0530

    948910: Extract Text  Sample updated

---
+++ How to/Extract Text/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <button v-on:click="extractText">extractText</button>
+    <button v-on:click="extractsText">extractsText</button>
+    <ejs-pdfviewer
+      id="pdfViewer"
+      ref="pdfviewer"
+      :documentPath="documentPath"
+      :resourceUrl="resourceUrl"
+      style="height: 640px;"
+    >
+    </ejs-pdfviewer>
+  </div>
+</template>
+
+<script>
+import {
+  PdfViewerComponent,
+  Toolbar,
+  Magnification,
+  Navigation,
+  Annotation,
+  TextSelection,
+  TextSearch,
+  FormFields,
+  FormDesigner,
+  PageOrganizer,
+} from '@syncfusion/ej2-vue-pdfviewer';
+
+export default {
+  name: 'App',
+  components: {
+    'ejs-pdfviewer': PdfViewerComponent,
+  },
+  data() {
+    return {
+      documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
+      resourceUrl: 'https://cdn.syncfusion.com/ej2/29.1.33/dist/ej2-pdfviewer-lib'
+    };
+  },
+  provide: {
+    PdfViewer: [
+      Toolbar,
+      Magnification,
+      Navigation,
+      Annotation,
+      TextSelection,
+      TextSearch,
+      FormFields,
+      FormDesigner,
+      PageOrganizer,
+    ],
+  },
+  methods: {
+    extractText() {
+      if (this.$refs.pdfviewer) {
+        const viewer = this.$refs.pdfviewer.ej2Instances;
+        viewer.extractText(1, 'TextOnly').then((val) => {
+          console.log('Extracted Text from Page 1:');
+          console.log(val);
+        });
+      }
+    },
+    extractsText() {
+      if (this.$refs.pdfviewer) {
+        const viewer = this.$refs.pdfviewer.ej2Instances;
+        viewer.extractText(0, 2, 'TextOnly').then((val) => {
+          console.log('Extracted Text from Pages 0 to 2:');
+          console.log(val);
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+</style>
